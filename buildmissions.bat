d8cb637ba6750824ad5cec3bda628c0d56abfa17
Nels0 <nelson.cook98@gmail.com>
2017-01-31 17:24:55 +1300

    Added util folder exceptions to build script

--- buildmissions.bat
+++ buildmissions.bat
@@ -1,5 +1,11 @@
 @ECHO off
-FOR /f "delims=" %%g in ('dir /a:d-h /b') do (
-    armake build --force %%g release/%%g.pbo  -w unquoted-string
-)
+setlocal enableDelayedExpansion 
+FOR /f "delims=" %%g in ('dir /a:d-h /b') do ( 
+	IF /I NOT %%g==release ( 
+    	IF /I NOT %%g==master_docs (
+    	    armake build -w unquoted-string --force %%g release/%%g.pbo  
+    	    echo built %%g 
+    	)
+    )
+) 
 
