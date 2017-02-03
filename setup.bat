635e49680b0fd86e405ae78f6a1d25d1c02dfa83
Nels0 <nelson.cook98@gmail.com>
2017-02-03 19:25:05 +1300

    tweak/fix setup script

--- setup.bat
+++ setup.bat
@@ -1,8 +1,12 @@
 @ECHO off
-FOR /f "delims=" %%g in ('dir /a:d-h /b') do (
-    del %%g\description.ext
-    mklink /h %%g\description.ext master_docs\description.ext
-    del %%g\initserver.sqf
-    mklink /h %%g\description.ext master_docs\initiserver.sqf
-)
+FOR /f "delims=" %%g in ('dir /a:d-h /b') do ( 
+	IF /I NOT %%g==release ( 
+    	IF /I NOT %%g==master_docs (
+    		del %%g\description.ext
+		    mklink /h %%g\description.ext master_docs\description.ext
+		    del %%g\initServer.sqf
+		    mklink /h %%g\initServer.ext master_docs\initServer.sqf
+    	)
+    )
+) 
 
